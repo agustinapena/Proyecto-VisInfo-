b7ddd850d80c0ba92f96f82c3aa39b660a2594b7
aruiz19 <aruizm@uc.cl>
2024-06-17 19:33:45 -0400

    mapa

--- main.js
+++ main.js
@@ -9,11 +9,20 @@ const margin = { top: 50, right: 50, bottom: 50, left: 50 };
 const SVG1 = d3.select("#vis-1").append("svg")
     .attr("width", WIDTH_VIS_1)
     .attr("height", HEIGHT_VIS_1)
-  .append("g")
+    .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Cargar datos desde el archivo CSV
 d3.csv(SISMOS).then(data => {
+    //REVISAR!!!!
+    data = data.map(d => ({
+        ...d,
+        latitude: parseFloat(d.latitude),
+        longitude: parseFloat(d.longitude),
+        mag:  parseFloat(d.mag),
+        depth: parseFloat(d.depth)
+    }));
+    
     // Definir intervalos de magnitud y tipos de magnitud
     const magnitudes = ["[2.5,3.7)", "[3.7,4.9)", "[4.9,6.1]"];
     const magTypes = ['md', 'mb_lg', 'ml', 'mwr', 'mw', 'mww', 'mb'];
@@ -127,6 +136,225 @@ d3.csv(SISMOS).then(data => {
         .attr("width", xScale.bandwidth() / 7)
         .attr("height", d => HEIGHT_VIS_1 - margin.top - margin.bottom - yScale(d.count))
         .attr("fill", d => colors[d.type]);
+
+    // Visualización 2 - Mapa
+    const WIDTH_VIS_2 = 1000;
+    const HEIGHT_VIS_2 = 500;
+
+    const SVG2 = d3.select("#vis-2").append("svg")
+        .attr("width", WIDTH_VIS_2)
+        .attr("height", HEIGHT_VIS_2)
+
+    const projection = d3.geoNaturalEarth1()
+        .translate([WIDTH_VIS_2 / 2, HEIGHT_VIS_2 / 2])
+        .scale(150);
+
+    const path = d3.geoPath().projection(projection);
+
+    d3.json("https://d3js.org/world-110m.v1.json").then(world => {
+    SVG2.append("path")
+        .datum(topojson.feature(world, world.objects.countries))
+        .attr("d", path)
+        .attr("fill", "#e0e0e0")
+        .attr("stroke", "#ffffff");
+    
+    const sismosGroup = SVG2.append("g");
+
+    // Añadir sismos al mapa
+    sismosGroup.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => projection([d.longitude, d.latitude])[0])
+        .attr("cy", d => projection([d.longitude, d.latitude])[1])
+        .attr("r", d => Math.sqrt(d.mag)) // El radio del círculo depende de la magnitud
+        .attr("fill", d => colors[d.magType])
+        .attr("opacity", 0.7)
+        .append("title") // Tooltip con la información del sismo
+        .text(d => `Lugar: ${d.place}\nMagnitud: ${d.mag}\nProfundidad: ${d.depth}`);
+});
+
+    // Añadir zoom y pan al mapa
+    SVG2.call(d3.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", ({ transform }) => {
+            sismosGroup.attr("transform", transform);
+            sismosGroup.attr("stroke-width", 1 / transform.k);
+        }));
+
+    // cargarDatosMapa("md");
+
+    // d3.json("https://d3js.org/world-110m.v1.json").then(world => {
+    //     sismosGroup.append("path")
+    //         .datum(topojson.feature(world, world.objects.land))
+    //         .attr("d", path)
+    //         .attr("fill", "#f0f0f0");
+
+    //     sismosGroup.selectAll("circle")
+    //         .data(data)
+    //         .enter().append("circle")
+    //         .attr("cx", d => projection([d.longitude, d.latitude])[0])
+    //         .attr("cy", d => projection([d.longitude, d.latitude])[1])
+    //         .attr("r", d => Math.sqrt(d.mag))
+    //         .attr("fill", d => colors[d.magType])
+    //         .attr("opacity", 0.7)
+    //         .append("title")
+    //         .text(d => `Magnitud: ${d.mag}, Tipo: ${d.magType}`);
+    // });
+
+    // SVG2.call(d3.zoom().on("zoom", () => {
+    //     sismosGroup.attr("transform", d3.event.transform);
+    // })); 
+    
+    // Función para cargar y mostrar los datos iniciales del mapa
+    // function cargarDatosMapa(magnitudeType) {
+    //     d3.csv(SISMOS).then(data => {
+    //         // Filtrar datos según el tipo de magnitud seleccionado
+    //         const filteredData = data.filter(d => d.magType === magnitudeType);
+
+    //         // Eliminar los puntos existentes en el mapa
+    //         sismosGroup.selectAll("circle").remove();
+
+    //         // Actualizar puntos en el mapa
+    //         sismosGroup.selectAll("circle")
+    //             .data(filteredData)
+    //             .enter().append("circle")
+    //             .attr("cx", d => projection([d.longitude, d.latitude])[0])
+    //             .attr("cy", d => projection([d.longitude, d.latitude])[1])
+    //             .attr("r", d => Math.sqrt(d.mag))
+    //             .attr("fill", d => colors[d.magType])
+    //             .attr("opacity", 0.7)
+    //             .append("title")
+    //             .text(d => `Magnitud: ${d.mag}, Tipo: ${d.magType}`);
+    //     }).catch(error => {
+    //         console.error('Error cargando los datos:', error);
+    //     });
+    // }
+
+    // // Función para actualizar el mapa cuando se hace clic en un botón
+    // function actualizarMapa(magnitudeType) {
+    //     // Cargar y mostrar datos actualizados según el tipo de magnitud seleccionado
+    //     cargarDatosMapa(magnitudeType);
+    // }
+
+    // // Asignar eventos de clic a los botones de magnitud
+    // d3.select("#btn1").on("click", () => actualizarMapa("md"));
+    // d3.select("#btn2").on("click", () => actualizarMapa("mwr"));
+    // d3.select("#btn3").on("click", () => actualizarMapa("mw"));
+    // d3.select("#btn4").on("click", () => actualizarMapa("ml"));
+    // d3.select("#btn5").on("click", () => actualizarMapa("mb"));
+    // d3.select("#btn6").on("click", () => actualizarMapa("mww"));
+    // d3.select("#btn7").on("click", () => actualizarMapa("mb_lg"));
+
+    // Visualización 3 - Gráfico de dispersión
+    const WIDTH_VIS_3 = 1000;
+    const HEIGHT_VIS_3 = 500;
+
+    const SVG3 = d3.select("#vis-3").append("svg")
+        .attr("width", WIDTH_VIS_3)
+        .attr("height", HEIGHT_VIS_3)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    const xScale3 = d3.scaleLinear()
+        .range([0, WIDTH_VIS_3 - margin.left - margin.right]);
+
+    const yScale3 = d3.scaleLinear()
+        .range([HEIGHT_VIS_3 - margin.top - margin.bottom, 0]);
+
+    const xAxis3 = d3.axisBottom(xScale3);
+    const yAxis3 = d3.axisLeft(yScale3);
+
+    xScale3.domain(d3.extent(data, d => d.mag)).nice();
+    yScale3.domain(d3.extent(data, d => d.depth)).nice();
+
+    SVG3.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0, ${HEIGHT_VIS_3 - margin.top - margin.bottom})`)
+        .call(xAxis3)
+        .append("text")
+        .attr("x", (WIDTH_VIS_3 - margin.left - margin.right) / 2)
+        .attr("y", 40)
+        .attr("fill", "gray")
+        .attr("font-size", "16px")
+        .attr("font-family", "Lato, sans-serif")
+        .style("text-anchor", "middle")
+        .text("Magnitud");
+    
+    SVG3.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis3)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (HEIGHT_VIS_3 / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("font-size", "16px")
+        .style("font-family", "Lato, sans-serif")
+        .style("fill", "gray")
+        .text("Profundidad");
+
+    const circles = SVG3.selectAll("circle")
+        .data(data)
+        .enter().append("circle")
+        .attr("cx", d => xScale3(d.mag))
+        .attr("cy", d => yScale3(d.depth))
+        .attr("r", 5)
+        .attr("fill", d => colors[d.magType])
+        .attr("opacity", 0.7)
+
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    circles.on("mouseover", d => {
+        tooltip.transition()
+            .duration(200)
+            .style("opacity", .9);
+        tooltip.html(`Magnitud: ${d.mag}<br>Profundidad: ${d.depth}`)
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+    }).on("mouseout", d => {
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+    });
+
+    function actualizarDispersion(magnitudeType) {
+        const filteredData = data.filter(d => magnitudeType.includes(d.magType));
+
+        xScale3.domain(d3.extent(filteredData, d => d.mag)).nice();
+        yScale3.domain(d3.extent(filteredData, d => d.depth)).nice();
+
+        SVG3.select(".x-axis")
+            .transition()
+            .duration(1000)
+            .call(xAxis3);
+
+        SVG3.select(".y-axis")
+            .transition()
+            .duration(1000)
+            .call(yAxis3);
+
+        circles.data(filteredData)
+            .transition()
+            .duration(1000)
+            .attr("cx", d => xScale3(d.mag))
+            .attr("cy", d => yScale3(d.depth))
+            .attr("fill", d => colors[d.magType]);
+    }
+
+    // Asignar eventos de clic a los botones de magnitud
+    d3.select("#btn1").on("click", () => actualizarDispersion(["md"]));
+    d3.select("#btn2").on("click", () => actualizarDispersion(["mwr"]));
+    d3.select("#btn3").on("click", () => actualizarDispersion(["mw"]));
+    d3.select("#btn4").on("click", () => actualizarDispersion(["ml"]));
+    d3.select("#btn5").on("click", () => actualizarDispersion(["mb"]));
+    d3.select("#btn6").on("click", () => actualizarDispersion(["mww"]));
+    d3.select("#btn7").on("click", () => actualizarDispersion(["mb_lg"]));
+
+    // Llamar a la función inicial para cargar los datos del gráfico al inicio
+    actualizarDispersion(["md"]);
 
 }).catch(error => {
     console.error('Error cargando los datos:', error);
